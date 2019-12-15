db5140b43fc68c9aae60138fccb36ecf3db32525
soufya <benkhalefs@gmail.com>
2019-12-15 17:04:58 +0000

    fixe

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,14 @@
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 
+# login AWS ECR
+eval $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email) 
+
+
+docker tag netflux:latest $IMAGE_REPO_URL:latest
+docker push $IMAGE_REPO_URL:latest
+
+
 # install necessary dependency for ecs-deploy
 add-apt-repository ppa:eugenesan/ppa
 apt-get update
@@ -11,13 +19,6 @@ apt-get install jq -y
 curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
   sudo tee -a /usr/bin/ecs-deploy
 sudo chmod +x /usr/bin/ecs-deploy
-
-# login AWS ECR
-eval $(aws ecr get-login --region us-east-2)
-
-
-docker tag netflux:latest $IMAGE_REPO_URL:latest
-docker push $IMAGE_REPO_URL:latest
 
 # update an AWS ECS service with the new image
 ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
